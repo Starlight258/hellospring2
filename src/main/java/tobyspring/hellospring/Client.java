1b2c6601ef48920debe73a66c06dbef112722c7d
Mint <audwl03071@gmail.com>
2025-01-05 10:37:31 +0900

    feat: 고정 환율 서비스 구현체 생성

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -3,12 +3,12 @@ package tobyspring.hellospring;
 import java.io.IOException;
 import java.math.BigDecimal;
 import tobyspring.hellospring.service.PaymentService;
-import tobyspring.hellospring.service.WebApiExRatePaymentService;
+import tobyspring.hellospring.service.SimpleExRatePaymentService;
 
 public class Client {
 
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new WebApiExRatePaymentService();
+        PaymentService paymentService = new SimpleExRatePaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
