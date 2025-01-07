faa22b3a2cf0cc72271191c5f2f5b6aa8b60ee68
Mint <audwl03071@gmail.com>
2025-01-07 22:26:38 +0900

    refactor: 스프링 사용하도록 전환

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,15 +1,20 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import tobyspring.hellospring.provider.ExRateProvider;
 import tobyspring.hellospring.provider.SimpleExRateProvider;
 import tobyspring.hellospring.service.PaymentService;
 
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
     }
