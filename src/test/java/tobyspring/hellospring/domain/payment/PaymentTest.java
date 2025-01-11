ee35dd4aee1c02242042362b07500cba00948cf0
Mint <audwl03071@gmail.com>
2025-01-12 00:00:17 +0900

    test: 유효 시간 검증 테스트 추가

--- src/test/java/tobyspring/hellospring/domain/payment/PaymentTest.java
+++ src/test/java/tobyspring/hellospring/domain/payment/PaymentTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PaymentTest {
@@ -25,6 +28,23 @@ class PaymentTest {
         Assertions.assertAll(
                 () -> assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000)),
                 () -> assertThat(payment.getValidUntil()).isEqualTo(LocalDateTime.now(clock).plusMinutes(30))
+        );
+    }
+
+    @Test
+    @DisplayName("유효시간은 현재 시간의 30분까지이다.")
+    void 유효시간은_현재_시간의_30분까지이다() {
+        // Given
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        // When
+        Payment payment = Payment.createPrepared(1L, "USD", BigDecimal.TEN, BigDecimal.valueOf(1_000),
+                LocalDateTime.now(clock));
+
+        // Then
+        Assertions.assertAll(
+                () -> assertThat(payment.isValid(clock)).isTrue(),
+                () -> assertThat(payment.isValid(Clock.offset(clock, Duration.of(30, ChronoUnit.MINUTES)))).isFalse()
         );
     }
 }
