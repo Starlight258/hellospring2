dc2cd8f0db34f37eafb2bbb805f5f63386e0611c
Mint <audwl03071@gmail.com>
2025-01-11 22:40:14 +0900

    test: 스프링 DI 테스트 작성

--- src/test/java/tobyspring/hellospring/config/TestPaymentConfig.java
+++ src/test/java/tobyspring/hellospring/config/TestPaymentConfig.java
@@ -1,6 +1,9 @@
 package tobyspring.hellospring.config;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tobyspring.hellospring.domain.payment.ExRateProvider;
@@ -8,15 +11,20 @@ import tobyspring.hellospring.domain.payment.ExRateProviderStub;
 import tobyspring.hellospring.domain.payment.PaymentService;
 
 @Configuration
-public class TestObjectFactory {
+public class TestPaymentConfig {
 
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
+        return new PaymentService(exRateProvider(), clock());
     }
 
     @Bean
     public ExRateProvider exRateProvider() {
         return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.fixed(Instant.now(), ZoneId.systemDefault());
     }
 }
--- src/test/java/tobyspring/hellospring/domain/payment/PaymentServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/domain/payment/PaymentServiceSpringTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,14 +13,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tobyspring.hellospring.config.TestObjectFactory;
+import tobyspring.hellospring.config.TestPaymentConfig;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestObjectFactory.class)
+@ContextConfiguration(classes = TestPaymentConfig.class)
 class PaymentServiceSpringTest {
 
     @Autowired
     PaymentService paymentService;
+
+    @Autowired
+    Clock clock;
 
     @Test
     @DisplayName("변환된 금액을 테스트한다.")
@@ -32,5 +36,19 @@ class PaymentServiceSpringTest {
                 () -> assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now()),
                 () -> assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30))
         );
+    }
+
+    @Test
+    @DisplayName("유효시간은 현재 시간의 30분 뒤이다.")
+    void 유효시간은_현재_시간의_30분_뒤이다() throws IOException {
+        // Given
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // When
+        LocalDateTime now = LocalDateTime.now(this.clock);
+        LocalDateTime expectedValidUntil = now.plusMinutes(30);
+
+        // Then
+        assertThat(payment.getValidUntil()).isEqualTo(expectedValidUntil);
     }
 }
