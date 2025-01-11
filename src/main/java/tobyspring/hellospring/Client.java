3608497840eb643ccbcd750c45fa8557a0f6bcf7
Mint <audwl03071@gmail.com>
2025-01-11 22:39:40 +0900

    feat: Clock 도입
    - 테스트하기 좋은 설계를 위해 Clock 도입

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -5,14 +5,14 @@ import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import tobyspring.hellospring.config.ObjectFactory;
+import tobyspring.hellospring.config.PaymentConfig;
 import tobyspring.hellospring.domain.payment.Payment;
 import tobyspring.hellospring.domain.payment.PaymentService;
 
 public class Client {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(PaymentConfig.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
--- src/main/java/tobyspring/hellospring/config/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/config/PaymentConfig.java
@@ -1,21 +1,22 @@
 package tobyspring.hellospring.config;
 
+import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import tobyspring.hellospring.Client;
 import tobyspring.hellospring.domain.exrate.CachedExRateProvider;
-import tobyspring.hellospring.domain.payment.ExRateProvider;
 import tobyspring.hellospring.domain.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.domain.payment.ExRateProvider;
 import tobyspring.hellospring.domain.payment.PaymentService;
 
 @Configuration
 @ComponentScan(basePackageClasses = Client.class)  // 메인 애플리케이션 클래스 기준
-public class ObjectFactory {
+public class PaymentConfig {
 
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(cachedExRateProvider());
+        return new PaymentService(cachedExRateProvider(), clock());
     }
 
     @Bean
@@ -26,5 +27,10 @@ public class ObjectFactory {
     @Bean
     public ExRateProvider exRateProvider() {
         return new WebApiExRateProvider();
+    }
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
     }
 }
