767e9c52f2717f97da4c59922a7d0b43be7a398b
Mint <audwl03071@gmail.com>
2025-01-16 23:20:43 +0900

    chore: DataConfig에 DB 관련 빈 등록

---
+++ src/main/java/tobyspring/hellospring/config/DataConfig.java
@@ -0,0 +1,34 @@
+package tobyspring.hellospring.config;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfig {
+
+    private static final String PACKAGE_NAME = "tobyspring.hellospring";
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan(PACKAGE_NAME);
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
+            setDatabase(Database.H2);
+            setGenerateDdl(true);
+            setShowSql(true);
+        }});
+        return emf;
+    }
+}
