86ac4542e69d1415681d5d71227dc3be5002f42f
Mint <audwl03071@gmail.com>
2025-01-16 23:30:36 +0900

    refactor: 사용하지 않은 메인 메서드 제거

--- src/main/java/tobyspring/hellospring/domain/sort/Sort.java
+++ src/main/java/tobyspring/hellospring/domain/sort/Sort.java
@@ -1,7 +1,5 @@
 package tobyspring.hellospring.domain.sort;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,12 +8,5 @@ public class Sort {
     public List<String> sortByLength(List<String> list) {
         list.sort(Comparator.comparingInt(String::length));
         return list;
-    }
-
-    public static void main(String[] args) {
-        List<String> scores = Arrays.asList("z", "x", "spring", "java");
-        Collections.sort(scores, Comparator.comparingInt(String::length));
-
-        scores.forEach(System.out::println);
     }
 }
