7fd517fbe430a6a1ea67a3c20a2f0d7871a09c4a
Mint <audwl03071@gmail.com>
2025-01-28 23:57:57 +0900

    test: 트랜잭션 테스트 작성
    - 롤백 여부를 검증하기 위해 중간에 잘못된 주문을 생성하여 DB에 저장된 주문 개수를 확인한다.

--- src/test/java/tobyspring/hellospring/domain/order/OrderServiceTest.java
+++ src/test/java/tobyspring/hellospring/domain/order/OrderServiceTest.java
@@ -1,10 +1,18 @@
 package tobyspring.hellospring.domain.order;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import java.math.BigDecimal;
-import org.assertj.core.api.Assertions;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tobyspring.hellospring.config.OrderConfig;
@@ -16,15 +24,59 @@ class OrderServiceTest {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void createOrder() {
+    void 성공_주문생성() {
         // Given
 
         // When
         Order savedOrder = orderService.createOrder("0100", BigDecimal.TEN);
 
         // Then
-        Assertions.assertThat(savedOrder).extracting("no", "total")
+        assertThat(savedOrder).extracting("no", "total")
                 .containsExactly("0100", BigDecimal.TEN);
+    }
+
+    @Test
+    void 성공_여러주문생성() {
+        // Given
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.ONE),
+                new OrderReq("0201", BigDecimal.TWO)
+        );
+
+        // When
+        List<Order> orders = orderService.createOrders(orderReqs);
+
+        // Then
+        assertAll(
+                () -> assertThat(orders).hasSize(2),
+                () -> orders.forEach(order -> assertThat(order.getId()).isGreaterThan(0))
+        );
+    }
+
+    @Test
+    void 실패_여러주문생성_트랜잭션내중복key() {
+        // Given
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.TWO)
+        );
+
+        // When & Then
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                        .isInstanceOf(DataIntegrityViolationException.class),
+                () -> assertThat(getCount("0300")).isEqualTo(0)
+        );
+    }
+
+    private Long getCount(final String key) {
+        JdbcClient client = JdbcClient.create(dataSource);
+        return client.sql("select count(*) from orders where no = " + key)
+                .query(Long.class)
+                .single();
     }
 }
